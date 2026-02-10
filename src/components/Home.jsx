e3a432241afee7d156071d37ea499acf3231d957
0xyusufz <gurucoc471@gmail.com>
2026-02-10 20:12:33 +0530

    changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,6 @@ const Home = () => {
                 <p>Solution to all your problems</p>
             </main>
         </div>
-          <h1>erfg</h1>
         <div className="home2">
             <img src="/" alt="Graphics" /> 
         </div>
