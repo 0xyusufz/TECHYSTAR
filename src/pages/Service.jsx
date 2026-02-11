d7e45223af7a0deebaec3d4014476c250a22d10b
0xyusufz <gurucoc471@gmail.com>
2026-02-11 20:22:03 +0530

    service page added using carousel

---
+++ src/pages/Service.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Carousel} from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import img1 from '../assets/3.jpg'
+import img2 from '../assets/4.jpg'
+
+
+const About = () => {
+  return (
+    <div>
+      <Carousel infiniteLoop autoPlay showStatus={false} showArrows={false} interval={2000} showThumbs={false} >
+        <div>
+          <img src={img1} alt="item2" />
+          <p className='legend'>Full Stack</p>
+        </div>
+        <div>
+          <img src={img2} alt="item3" />
+          <p className='legend'>24x7 Support</p>
+        </div>
+      </Carousel>
+    </div>
+  )
+}
+
+export default About
